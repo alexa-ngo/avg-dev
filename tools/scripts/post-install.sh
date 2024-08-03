fae2bc73f62c4efcafdc9d5881e984f34b94dbd4
Alexa <alexaariango@outlook.com>
2024-08-02 22:41:57 -0700

    Updates

--- tools/scripts/post-install.sh
+++ tools/scripts/post-install.sh
@@ -5,9 +5,12 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install curl vim build-essential gnome-tweaks git -y
 
+GitHub Repository: https://github.com/alexa-ngo/avg-dev/
 
 # Install Volta (volta.sh)
 curl https://get.volta.sh | bash
 
 # Install node using volta from the absolute path
 $HOME/.volta/bin/volta install node@lts
+
+echo "All done! You should run this command to finish up: source ~/.bashrc" 
