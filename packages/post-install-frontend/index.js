4868011ad683864aebbb63f5e5cadc1808c9674d
Alexa <alexaariango@outlook.com>
2024-08-06 15:41:32 -0700

    Post install frontend

---
+++ packages/post-install-frontend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || "https://raw.githubusercontent.com/alexa-ngo/avg-dev/main/tools/scripts/post-install.sh";
+
+app.get("/", function(req, res){
+    return res.send(`wget -qO- ${URL} | bash`);
+});
+
+
+app.listen(PORT, function() {
+    console.log(`Server is listening on: ${PORT}`);
+});
+
+
